6da151198196abe9163bc4af5f0d06ef13ac9ab7
Junior_Kamanda <jkamanda062gmail.com>
2024-09-23 16:38:24 +0200

    Joke_App_end

--- joke-app/index.js
+++ joke-app/index.js
@@ -3,13 +3,10 @@ const DisplayJoke = () => {
   fetch("https://api.blablagues.net/?rub=blagues")
     .then((res) => res.json())
     .then((data) => {
-      const joke_title = data.data.content.text_head;
-      const joke_para = data.data.content.text;
+      const joke = data.data.content;
 
-      header.textContent = joke_title;
-      content.textContent = joke_para
-        ? joke_para
-        : joke_para.text_hidden;
+      header.textContent = joke.text_head;
+      content.textContent = joke.text ? joke.text : joke.text_hidden;
     })
     .catch((err) => console.log(err));
 };
