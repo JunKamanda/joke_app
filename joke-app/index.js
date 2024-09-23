a0305a6e4334facb1dd0a79f05a400808be026d3
Junior_Kamanda <jkamanda062gmail.com>
2024-09-23 16:22:03 +0200

    Joke_App_end

--- joke-app/index.js
+++ joke-app/index.js
@@ -1,4 +1,19 @@
-// const header = document.getElementById("header")
-// const body = document.querySelector("body")
+const DisplayJoke = () => {
+  // https://api.blablagues.net/?rub=blagues
+  fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+      const joke_title = data.data.content.text_head;
+      const joke_para = data.data.content.text;
 
-// https://api.blablagues.net/?rub=blagues
+      header.textContent = joke_title;
+      content.textContent = joke_para
+        ? joke_para
+        : joke_para.text_hidden;
+    })
+    .catch((err) => console.log(err));
+};
+
+DisplayJoke();
+
+document.body.addEventListener("click", DisplayJoke);
