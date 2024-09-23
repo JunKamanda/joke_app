70d5709dc3301cd03dbe74e432ca0ddaabbdacce
Junior_Kamanda <jkamanda062gmail.com>
2024-09-23 15:04:18 +0200

    Joke_App

--- joke-app/index.js
+++ joke-app/index.js
@@ -2,17 +2,3 @@
 // const body = document.querySelector("body")
 
 // https://api.blablagues.net/?rub=blagues
-function getJoke(){
-fetch("https://api.blablagues.net/?rub=blagues")
-  .then((res) => res.json())
-  .then((data) => {
-
-    const joke = data.data.content;
-
-    header.textContent = joke.text_head
-    content.textContent = joke.text !== "" ? joke.text : joke.text_hidden;
-  } )
-  .catch((err) => console.log(err));
-}
-getJoke()
-document.body.addEventListener("click", getJoke)
